5c74ab83f8b62f1920c2ab4d2bb9dcaf5d066260
rampazio <diegorfreitas3009@gmail.com>
2022-01-12 22:26:55 -0300

    ExemploConstrutores

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Leonardo", "Buta" );
+            p1.Apresentar();
+        }
+    }
+}
+
+
+
